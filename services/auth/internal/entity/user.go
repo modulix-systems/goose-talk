27dbb90c7f7e0eb090cdb4d8b17eb4cf1c8fb6b3
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-05-16 15:42:14 +0200

    feat(auth): new entities: SignUpCode and TwoFactorAuth
    SignUpCode will be used for email confirmation on signup.
    And TwoFactorAuth is added to implement 2fa functionallity

--- services/auth/internal/entity/user.go
+++ services/auth/internal/entity/user.go
@@ -2,7 +2,22 @@ package entity
 
 import "time"
 
+type TwoFADeliveryMethod = int
+
+const (
+	TELEGRAM TwoFADeliveryMethod = iota
+	EMAIL
+	SMS
+)
+
 type (
+	// SignUpCode represents storage for email verifications codes
+	// Only one code can be present for one email
+	SignUpCode struct {
+		Code      string
+		Email     string
+		CreatedAt time.Time
+	}
 	User struct {
 		ID         int
 		Username   string
@@ -17,5 +32,16 @@ type (
 		IsActive   bool
 		BirthDate  time.Time
 		AboutMe    string
+	}
+	// TwoFactorAuth entity representing 2FA auth (if user has enabled it)
+	TwoFactorAuth struct {
+		UserId         int
+		DeliveryMethod TwoFADeliveryMethod
+		// could be whether user's telegram, email address or phone number
+		// depending on DeliveryMethod.
+		// The field can be optional e.g for email because it can be taken from user's acc
+		contact   string
+		otpSecret string
+		Enabled   bool
 	}
 )
