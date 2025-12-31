2567e513ab56526fecf7d32afce64592408e9f98
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-12-31 16:46:48 +0100

    refactor(auth): remove and update unnecessary migrations

--- services/auth/migrations/1749306736_initial.down.sql
+++ services/auth/migrations/1749306736_initial.down.sql
@@ -4,5 +4,7 @@ DROP TABLE IF EXISTS two_factor_auth;
 DROP TABLE IF EXISTS user_session;
 DROP TABLE IF EXISTS client_identity;
 DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS passkey_credential;
+DROP TYPE IF EXISTS two_fa_transport;
 
 COMMIT;
--- services/auth/migrations/1749306736_initial.up.sql
+++ services/auth/migrations/1749306736_initial.up.sql
@@ -14,7 +14,8 @@ CREATE TABLE IF NOT EXISTS "user" (
   updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
   is_active BOOL DEFAULT true NOT NULL,
   birth_date TIMESTAMPTZ,
-  about_me TEXT
+  about_me TEXT,
+  private_key TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS client_identity (
@@ -38,11 +39,21 @@ CREATE TABLE IF NOT EXISTS user_session (
 CREATE TYPE two_fa_transport AS ENUM ('telegram', 'email', 'sms', 'totp_app');
 
 CREATE TABLE IF NOT EXISTS two_factor_auth (
-  user_id INT PRIMARY KEY,
+  user_id INT PRIMARY KEY REFERENCES "user"(id),
   transport two_fa_transport NOT NULL,
   contact TEXT,
   totp_secret BYTEA,
   enabled BOOL DEFAULT true NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS passkey_credential (
+  id TEXT PRIMARY KEY,
+  user_id INT NOT NULL REFERENCES "user"(id),
+  public_key BYTEA NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  last_used_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  backed_up BOOL NOT NULL DEFAULT false,
+  transports TEXT[] NOT NULL
 );
 
 COMMIT;
